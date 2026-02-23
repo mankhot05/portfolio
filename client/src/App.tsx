a1d3bb05433a7a1452f123f4be60cd0dec137740
Mayank Hothur <mayankhothur@Mayanks-MacBook-Air.local>
2026-02-22 21:29:03 -0800

    React basics: components, props, state, lists

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+interface Project {
+  title: string
+  description: string
+}
+
+function ProjectCard(props: Project) {
+  return <div> 
+    <h2>
+      {props.title}
+    </h2>
+    <p>
+      {props.description}
+    </p>
+  </div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const projects = [
+    { title: "Project 1", description: "This is my first project"},
+    { title: "Project 2", description: "This is my second project"}
+  ]
+  return <div>
+    {projects.map((project) => (
+        <ProjectCard title={project.title} description={project.description} key={project.title} />
+    ))}
+</div>
 }
 
 export default App
+
+
